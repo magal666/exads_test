51944ad7a2eb0d9652d5f433e1ff2181d99ea04d
Vinicius Rodrigues da Silva <viniciusrs@rvdigitalbrasil.com.br>
2022-07-16 12:08:35 -0300

    I recreated the exercise because I have doubt if the exercise is a prank or not.

--- exercise_1/index.php
+++ exercise_1/index.php
@@ -1,5 +1,11 @@
 <?php
 
+//I created that flag below because the translate is dubious and usually when I do that kind of exercise, about prime number
+//I delivery a number "divisible", but the exercise asked me "multiple" number.
+//For that reason I did the both possible results and separated for that flag.
+//if the exercise is a prank, I print the result of a prank, else I print the result about prime numbers...LOL
+$GLOBALS['isPrank'] = false;
+
 function primeCheck($number){
 
     if ($number == 1){
@@ -18,35 +24,56 @@ function primeCheck($number){
 function multipleCheck($number){
     $datas = array();
 
-    for($i = 1; $i <= 100; $i++){
-        if($i % $number == 0){
-            $datas[] = $i;
+    if($GLOBALS['isPrank']){
+
+        //I use this because, first I think the exercise have a prank
+        for($i = 1; $i <= 100; $i++){
+            if($i % $number == 0){
+                $datas[] = $i;
+            }
         }
+
+    }else{
+
+
+        for($i = 1; $i <= $number; $i++) {
+            if ($number % $i == 0) {
+                $datas[] = $i;
+            }
+        }
+
     }
 
     return $datas;
 }
 
-/*for ($i = 1; $i <= 100; $i++){
+echo "<pre>";
 
-    $multiple = multipleCheck($i);
+for ($i = 1; $i <= 100; $i++){
 
-    if(count($multiple) > 1){
-        echo $i . " [" . implode(", ",  $multiple) . "]";
+    if(!$GLOBALS['isPrank']){
+
+        if(primeCheck($i)){
+            echo $i . " [PRIME]";
+        }else{
+            echo $i . " [" . implode(", ",  multipleCheck($i)) . "]";
+        }
+
     }else{
-        echo $i . " [PRIME]";
-    }
 
-    if(primeCheck($i)){
-       echo $i . " [PRIME]";
-    }else{
-       echo $i . " [" . implode(", ",  multipleCheck($i)) . "]";
+        //I use this because, first I think the exercise have a prank
+        $multiple = multipleCheck($i);
+
+        if(count($multiple) > 1){
+            echo $i . " [" . implode(", ",  $multiple) . "]";
+        }else{
+            echo $i . " [PRIME]";
+        }
     }
 
 
     echo "<br>";
 
-}*/
+}
 
-echo "teste";
 
